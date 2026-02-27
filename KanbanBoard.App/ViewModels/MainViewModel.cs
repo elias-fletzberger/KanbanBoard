5b028b1a3176837d88f4c076175e5cf59e7e3060
elias-fletzberger <elias-fletzberger@web.de>
2026-02-27 12:15:13 +0100

    Add MainViewModel Class

---
+++ KanbanBoard.App/ViewModels/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.ObjectModel;
+using KanbanBoard.Core.Models;
+using KanbanBoard.Infrastructure.Persistence;
+
+
+namespace KanbanBoard.App.ViewModels;
+
+public class MainViewModel
+{
+    private readonly InMemoryBoardRepository _repository;
+
+    public ObservableCollection<CardItem> Cards { get; set; }
+
+    public MainViewModel()
+    {
+        _repository = new InMemoryBoardRepository();
+
+        var board = _repository.Load();
+
+        Cards = new ObservableCollection<CardItem>(board.Cards);
+    }
+}
