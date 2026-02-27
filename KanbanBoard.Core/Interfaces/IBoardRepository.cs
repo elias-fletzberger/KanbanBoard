c80a2818ca16c4b5daae0be12fd48fc1e213f92b
elias-fletzberger <elias-fletzberger@web.de>
2026-02-27 11:43:21 +0100

    Add board repository abstraction and in-memory implementation

---
+++ KanbanBoard.Core/Interfaces/IBoardRepository.cs
@@ -0,0 +1,10 @@
+﻿using KanbanBoard.Core.Models;
+
+
+namespace KanbanBoard.Core.Interfaces;
+
+public interface IBoardRepository
+{
+    Board Load();
+    void Save(Board board);
+}
---
+++ KanbanBoard.Infrastructure/Persistence/InMemoryBoardRepository.cs
@@ -0,0 +1,20 @@
+﻿using KanbanBoard.Core.Interfaces;
+using KanbanBoard.Core.Models;
+
+
+namespace KanbanBoard.Infrastructure.Persistence;
+
+public class InMemoryBoardRepository : IBoardRepository
+{
+    private Board _board = new();
+
+    public Board Load()
+    {
+        return _board; 
+    }
+
+    public void Save(Board board)
+    {
+        _board = board;
+    }
+}
