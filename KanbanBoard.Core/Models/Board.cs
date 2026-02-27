949d6f7ec85bc0db60a54079ac306681555a7fb9
elias-fletzberger <elias-fletzberger@web.de>
2026-02-27 11:07:45 +0100

    Add Board Class and BoardTests UnitTest

---
+++ KanbanBoard.Core/Models/Board.cs
@@ -0,0 +1,12 @@
+﻿
+namespace KanbanBoard.Core.Models;
+
+public class Board
+{
+    public List<CardItem> Cards { get; set; }
+    
+    public Board()
+    {
+        Cards = new List<CardItem>();
+    }
+}
---
+++ KanbanBoard.Tests/BoardTests.cs
@@ -0,0 +1,28 @@
+﻿using KanbanBoard.Core.Models;
+using Xunit;
+
+
+namespace KanbanBoard.Tests;
+
+public class BoardTests
+{
+    [Fact]
+    public void New_Board_Should_Start_With_Empty_List()
+    {
+        var board = new Board();
+
+        Assert.NotNull(board.Cards);
+        Assert.Empty(board.Cards);
+    }
+
+    [Fact]
+    public void Should_Be_Able_To_Add_Card_To_Board()
+    {
+        var board = new Board();
+        var card = new CardItem("Test Aufgabe");
+
+        board.Cards.Add(card);
+
+        Assert.Single(board.Cards);
+    }
+}
