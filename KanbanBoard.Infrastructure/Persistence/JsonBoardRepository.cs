3c5c72e9be763b161f4c60f766e300bf4cd67094
elias-fletzberger <elias-fletzberger@web.de>
2026-03-11 23:27:53 +0100

    Add JsonBoardRepository Class for saving cards to a file

---
+++ KanbanBoard.Infrastructure/Persistence/JsonBoardRepository.cs
@@ -0,0 +1,56 @@
+﻿
+using System;
+using System.Text.Json;
+using KanbanBoard.Core.Interfaces;
+using KanbanBoard.Core.Models;
+
+namespace KanbanBoard.Infrastructure.Persistence;
+
+
+/// <summary>
+/// JSON implementation of IBoardRepository.
+/// Stores the board data from the current application runtime
+/// to persistent storage.
+/// </summary>
+public class JsonBoardRepository : IBoardRepository
+{
+    //private Board _board = new();
+    private static readonly string _folderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),"KanbanBoard");
+    private readonly string _filePath = Path.Combine(_folderPath, "board.json");
+
+    private readonly JsonSerializerOptions _jsonOptions = new() { WriteIndented = true };
+
+    public Board Load()
+    {
+        Board board = new Board();
+
+        if (File.Exists(_filePath))
+        {
+            string cardJson = File.ReadAllText(_filePath);
+            board = JsonSerializer.Deserialize<Board>(cardJson);
+
+            if (board == null)
+            {
+                return board = new Board();
+            }
+            else
+            {
+                return board;
+            }
+        }
+        
+
+        return board;
+    }
+
+    public void Save(Board board)
+    {
+        if (!File.Exists(_folderPath))
+        {
+            Directory.CreateDirectory(_folderPath);
+        }
+        
+        string jsonBoard = JsonSerializer.Serialize<Board>(board, _jsonOptions);
+        File.WriteAllText(_filePath, jsonBoard);
+    }
+}
