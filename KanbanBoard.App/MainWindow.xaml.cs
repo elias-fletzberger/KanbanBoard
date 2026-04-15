2efb5a2707f47655ca248310ba79484824633a2a
elias-fletzberger <elias-fletzberger@web.de>
2026-04-15 12:31:34 +0200

    Enable drag-and-drop for cards in ListBox. First step - strat drag.

--- KanbanBoard.App/MainWindow.xaml.cs
+++ KanbanBoard.App/MainWindow.xaml.cs
@@ -1,14 +1,33 @@
 ﻿using KanbanBoard.App.ViewModels;
+using KanbanBoard.Core.Models;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace KanbanBoard.App;
 
-
+/// <summary>
+/// Interaction logic for MainWindow.xaml to Drag & Drop cards.
+/// </summary>
 public partial class MainWindow : Window
 {
     public MainWindow()
     {
         InitializeComponent();
         DataContext = new MainViewModel();
+    }
+
+    private void ListBox_PreviewMouseMove(object sender, MouseEventArgs e)
+    {
+        if (e.LeftButton == MouseButtonState.Pressed)
+        {
+            if(sender is ListBox listBox)
+            {
+                if (listBox.SelectedItem is CardItem card)
+                {
+                    DragDrop.DoDragDrop(listBox, card, DragDropEffects.Move);
+                }
+            }
+        }
     }
 }
